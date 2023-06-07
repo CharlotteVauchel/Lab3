5800c6634ac55b41e92f4d8f2a3893b878a5d872
CharlotteVauchel <vauchelcharlotte@gmail.com>
2023-06-07 21:36:10 +0200

    Adding the methods

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,14 @@
 public class Client {
     public Order[] orders;
 
-    public void main();
+    public Client(int nbOrders){
+        orders = new Order[nbOrders];
+    }
+
+    public void addOrder(int numOrder){
+        orders[numOrder] = new Order(numOrder);
+        orders[numOrder].attach(new PriceObserver());
+        orders[numOrder].attach(new QuantityObserver());
+    }
+
 }
--- src/PriceObserver.java
+++ src/PriceObserver.java
@@ -1,4 +1,13 @@
 public class PriceObserver implements OrderObserver {
 
-    public void update(Order order);
+    public void update(Order order) {
+        if (order.getTotalPrice() > 200 && !order.discountAdded) {
+            order.totalPrice = order.getTotalPrice() - 20;
+            order.discountAdded = true ;
+        }
+        if (order.getTotalPrice() < 200 && order.discountAdded){
+            order.totalPrice = order.getTotalPrice() + 20;
+            order.discountAdded = false ;
+        }
+    }
 }
